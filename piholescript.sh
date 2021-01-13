1bc1f3d3288aef0dc238d343eeee568f17856e46
bccdanilo <37251269+bccdanilo@users.noreply.github.com>
2021-01-13 09:49:07 -0300

    Create piholescript.sh

---
+++ piholescript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+saida_pihole_status='pihole status'
+trigger = "listening"
+echo "saida do pihole :"
+echo ${saida_pihole_status}  #19 - 26 mais ou menos
+echo "--------------------"
+echo "palavra chave"
+echo ${trigger}
+
+
+#if [[ $trigger ==  ${saida_pihole_status:19:26} ]]
+#then 
+#    pihole restartdns
+#fi
+
+# */1 * * * * root /home/script/piholescript.sh 
